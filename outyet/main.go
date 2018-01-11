765a1547b7e9de92b2694cc072e352b39e0b302f
xhyspring <zjqspring@163.com>
2018-01-11 19:03:52 +0800

    Update main.go

--- outyet/main.go
+++ outyet/main.go
@@ -33,7 +33,7 @@ import (
 var (
 	httpAddr   = flag.String("http", ":8080", "Listen address")
 	pollPeriod = flag.Duration("poll", 5*time.Second, "Poll period")
-	version    = flag.String("version", "2.01", "Go version")
+	version    = flag.String("version", "3.01", "Go version")
 )
 
 const baseChangeURL = "https://go.googlesource.com/go/+/"
